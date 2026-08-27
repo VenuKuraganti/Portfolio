9f491358d593abca7e2b3d2b6f8500ec0fa2945f
venu <venugopalakrishnakuraganti@gmail.com>
2026-08-27 17:46:53 +0530

    vite config commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Portfolio/',
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> 9f67e94a84f22a9ca1eb15993e6078dacda97822
